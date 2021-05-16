c01d5121543ad09433a290b401fab9ae372ce9cf
Virjanand <virjanand@virjanand.com>
2021-05-16 09:28:43 +0200

    Create tests for Alarm

--- TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -1,26 +1,45 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-public class Alarm
-{
-    private final double LowPressureThreshold = 17;
-    private final double HighPressureThreshold = 21;
+public class Alarm {
+    private static final double LOW_PRESSURE_THRESHOLD = 17;
+    private static final double HIGH_PRESSURE_THRESHOLD = 21;
 
-    Sensor sensor = new Sensor();
+    private Sensor sensor;
 
-    boolean alarmOn = false;
+    private boolean alarmOn;
 
-    public void check()
-    {
+    // Constructor for backwards compatibility
+    public Alarm() {
+        sensor = new Sensor();
+    }
+
+    // For unit testing
+    Alarm(Sensor sensor) {
+        this.sensor = new Sensor();
+        this.sensor = sensor;
+    }
+
+    public void check() {
         double psiPressureValue = sensor.popNextPressurePsiValue();
 
-        if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
-        {
+        if (isPressureWrong(psiPressureValue)) {
             alarmOn = true;
         }
     }
 
-    public boolean isAlarmOn()
-    {
-        return alarmOn; 
+    public boolean isAlarmOn() {
+        return alarmOn;
+    }
+
+    private boolean isPressureWrong(double psiPressureValue) {
+        return isPressureTooLow(psiPressureValue) || isPressureTooHigh(psiPressureValue);
+    }
+
+    private boolean isPressureTooHigh(double psiPressureValue) {
+        return Double.compare(psiPressureValue, HIGH_PRESSURE_THRESHOLD) > 0;
+    }
+
+    private boolean isPressureTooLow(double psiPressureValue) {
+        return Double.compare(psiPressureValue, LOW_PRESSURE_THRESHOLD) < 0;
     }
 }
--- TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -1,14 +1,60 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
-public class AlarmTest {
+class AlarmTest {
 
     @Test
-    public void foo() {
+    void alarmStartsOff() {
         Alarm alarm = new Alarm();
         assertFalse(alarm.isAlarmOn());
     }
+
+    @Test
+    void pressureTooLow() {
+        Alarm alarm = createAlarmWithSensorMock(16.9);
+
+        alarm.check();
+
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    void pressureOkLowerBound() {
+        Alarm alarm = createAlarmWithSensorMock(17.0);
+
+        alarm.check();
+
+        assertFalse(alarm.isAlarmOn());
+    }
+
+    @Test
+    void pressureOkUpperBound() {
+        Alarm alarm = createAlarmWithSensorMock(21.0);
+
+        alarm.check();
+
+        assertFalse(alarm.isAlarmOn());
+    }
+
+    @Test
+    void pressureTooHigh() {
+        Alarm alarm = createAlarmWithSensorMock(21.1);
+
+        alarm.check();
+
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    private Alarm createAlarmWithSensorMock(double pressurePsiValue) {
+        Sensor sensorMock = mock(Sensor.class);
+        when(sensorMock.popNextPressurePsiValue()).thenReturn(pressurePsiValue);
+        return new Alarm(sensorMock);
+    }
 }
+
